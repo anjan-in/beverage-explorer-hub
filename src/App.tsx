0dee0f5a7807c44d6095d926139d5491a9af9eea
anjan-in <anjansen30@gmail.com>
2026-07-14 17:37:26 +0530

    feat: integrate enterprise React ErrorBoundary to capture runtime failures gracefully

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,29 @@ import { Favorites } from './pages/Favorites';
 import { FavoritesProvider } from './context/FavoritesContext';
 import { Navbar } from './components/Navbar';
 import { ThemeProvider } from './context/ThemeContext';
+import { ErrorBoundary } from './components/ErrorBoundary';
 
 function App() {
   return (
     <ThemeProvider>
       <FavoritesProvider>
         <BrowserRouter>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/explore" element={<Explore />} />
-            <Route path="/beverage/:id" element={<BeverageDetails />} />
-            <Route path="/favorites" element={<Favorites />} />
-          </Routes>
+          <Navbar /> 
+          
+          {/* Wrap all interactive page content switches inside the boundary */}
+          <ErrorBoundary>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/explore" element={<Explore />} />
+              <Route path="/beverage/:id" element={<BeverageDetails />} />
+              <Route path="/favorites" element={<Favorites />} />
+            </Routes>
+          </ErrorBoundary>
+          
         </BrowserRouter>
       </FavoritesProvider>
     </ThemeProvider>
   );
 }
+
 export default App;
